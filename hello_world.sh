6f7d610a2ae87cf769710bcfe7d78e0be56b083d
Wenxin Hu <wenxinhu@ufl.edu>
2024-09-13 10:23:14 -0400

    Move print list to a function

--- hello_world.sh
+++ hello_world.sh
@@ -2,7 +2,10 @@
 # This is our first script.
 echo 'Hello World!'
 
+function print_list(){
 #print the list of files in the directory
 echo 'These are the files in the directory: '
 ls
+}
 
+print_list
